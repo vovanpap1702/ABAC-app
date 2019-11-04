5eebec6e88cb321ef88ebb11126357032d23a339
Mintaka <spaceisperfect@gmail.com>
2019-11-04 20:35:02 +0300

    Add angular services.

--- ABAC.WebApp/Program.cs
+++ ABAC.WebApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ABAC.API
+namespace ABAC.WebApp
 {
     public class Program
     {
