2a5c8b6d81be28cf90cc8e60d8163c4bf478abf0
Uladzimir Paprotski <paprotski1702@gmail.com>
2019-11-25 02:09:37 +0300

    Add migration configurations

---
+++ ABAC.DAL/Configuration/ResourceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ABAC.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using AutoMapper;
+using System.Linq;
+
+namespace ABAC.DAL.Configuration
+{
+	public class ResourceConfiguration : IEntityTypeConfiguration<Resource>
+	{
+		public void Configure(EntityTypeBuilder<Resource> builder)
+		{
+			builder.ToTable("resource").HasKey(item => item.Id);
+			builder.Property(user => user.Name).HasColumnName("name");
+			builder.Property(user => user.Value).HasColumnName("value");
+			builder.HasMany(user => user.Attributes);
+		}
+	}
+}
---
+++ ABAC.DAL/Configuration/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ABAC.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using AutoMapper;
+using System.Linq;
+
+namespace ABAC.DAL.Configuration
+{
+	public class UserConfiguration : IEntityTypeConfiguration<User>
+	{
+		public void Configure(EntityTypeBuilder<User> builder)
+		{
+			builder.ToTable("user").HasKey(item => item.Id);
+			builder.Property(user => user.Login).HasColumnName("login");
+			builder.Property(user => user.Password).HasColumnName("password");
+			builder.Property(user => user.Name).HasColumnName("name");
+			builder.HasMany(user => user.Attributes);
+		}
+	}
+}
