be5d538e5d67e79f1e3ce8d682a643dac2089df2
Uladzimir Paprotski <paprotski1702@gmail.com>
2019-11-24 22:46:04 +0300

    Add DbContext

---
+++ ABAC.DAL/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ABAC.DAL.Entities;
+using ABAC.DAL.Configuration;
+
+namespace ABAC.DAL.Context
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Resource> Resources { get; set; }
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+			Database.Migrate();
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new UserConfiguration());
+			modelBuilder.ApplyConfiguration(new ResourceConfiguration());
+		}
+	}
+}
